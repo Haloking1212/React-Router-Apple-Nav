073f15128a82046a3df6f226dea9bc75684a0be9
Hector Valdez <hvaldez180@gmail.com>
2019-10-14 16:08:35 -0400

    added apple logo

---
+++ apple-nav/src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavWrapper from "./NavWrapper";
+
+export default function Nav() {
+    return(
+        <NavWrapper>
+            
+        </NavWrapper>
+    )
+}
---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function NavWrapper() {
+    return (
+        <div className="nav-wrapper">
+            <img src="https://cdn.pixabay.com/photo/2018/05/08/21/28/apple-3384010_960_720.png" alt="the apple logo" className="logo" />
+        </div>
+    )
+}
